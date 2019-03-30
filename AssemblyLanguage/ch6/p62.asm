09c1fd8c561edc3fe0a61120649dd178b25b1b84
yangxiaohei <yangxiaohei321123@163.com>
2019-03-30 16:36:59 +0800

    第 6 章代码，练习 dw 的使用

--- AssemblyLanguage/ch6/p62.asm
+++ AssemblyLanguage/ch6/p62.asm
@@ -1,20 +1,17 @@
 assume cs:code
 
 code segment
-    
-    dw 0123h, 4567h, 89abh, 0cdefh
-    
-    start :
-            mov ax, 0
+    dw 0123h, 0456h, 0789h, 0abch, 0edfh, 0fdeh, 0cbah, 0987h
+    start:
             mov bx, 0
-            mov cx, 10h
-          s:add ax, cs:[bx]
+            mov ax, 0
+            mov cx, 8
+          s:
+            add ax, cs:[bx]
             add bx, 2
             loop s
 
             mov ax, 4c00h
             int 21h
-
 code ends
-
 end start
--- AssemblyLanguage/ch6/p63.asm
+++ AssemblyLanguage/ch6/p63.asm
@@ -1,30 +1,25 @@
 assume cs:code
 
 code segment
-
     dw 0123h, 0456h, 0789h, 0abch, 0defh, 0fedh, 0cbah, 0987h
-    dw 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-
     start:
-            mov ax, cs
+            mov ax, 0
             mov ss, ax
-            mov sp, 30h
-            mov bx, 0
+            mov sp, 300h
 
             mov cx, 8
-         s:push cs:[bx]
+            mov bx, 0
+          s:push cs:[bx]
             add bx, 2
             loop s
 
+            mov bx, 0
             mov cx, 8
-            mov bx, 10h
-          s0:pop cs:[bx]
+         s0:pop cs:[bx]
             add bx, 2
-            loop s0 
+            loop s0
 
             mov ax, 4c00h
             int 21h
-
 code ends
-
 end start
