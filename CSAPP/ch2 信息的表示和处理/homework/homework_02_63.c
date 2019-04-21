4c0daf757d2795dcf3756d16d11307ceeb9f70bb
yangxiaohei <yangxiaohei321123@163.com>
2019-04-21 22:51:43 +0800

    这题挺难的...sra 比较难，另一个简单

---
+++ CSAPP/ch2 信息的表示和处理/homework/homework_02_63.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "../../bin_tool.h"
+
+unsigned srl(unsigned x, int k)
+{
+    /* Perform shift arithmetically */
+    unsigned xsra = (int) x >> k;
+
+    int w = sizeof(x) << 3;
+    int a = (1 << k) - 1;
+    a <<= (w - k);
+    xsra &= ~a;
+    return xsra;
+}
+
+int sra(int x, int k)
+{
+    /* Perform shift logically */
+    int xsrl = (unsigned) x >> k;
+
+    /*
+     * 这题挺难的...
+     * 最重要的思路是那个 (z + ~(z & xsral))
+     */
+    int w = sizeof(x) << 3;
+    int z = 1 << (w - k - 1);
+    int rmask = z - 1;
+    int lmask = ~rmask;
+    int right = rmask & xsrl;
+    int left = lmask & (z + ~(z & xsrl));
+    return left | right;
+}
+
+int main(int argc, char *argv[])
+{
+    /*
+     * 00000000 00000000 00000000 00000001
+     * 11111110 11010000 10010000 00000010
+     * 00000010 11010000 10010000 00000010
+     */
+    printf("%s\n", i32_(srl(tou32("10000000 00000000 00000000 00000000"), 31)));
+    printf("%s\n", i32(sra(toi32("11011010 00010010 00000000 01010101"), 5)));
+    printf("%s\n", i32_(sra(toi32("01011010 00010010 00000000 01010101"), 5)));
+}
