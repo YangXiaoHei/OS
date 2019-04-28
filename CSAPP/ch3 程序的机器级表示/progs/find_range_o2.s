beec29093680c7b6aa6f7d7ec5864c85a34d12a1
yangxiaohei <yangxiaohei321123@163.com>
2019-04-29 00:58:24 +0800

    浮点数比较优化 o2 的代码

--- CSAPP/ch3 程序的机器级表示/progs/find_range_o2.s
+++ CSAPP/ch3 程序的机器级表示/progs/find_range_o2.s
@@ -4,18 +4,18 @@
 	.p2align	4, 0x90
 _find_range:                            ## @find_range
 	pushq	%rbp
-	movq	%rsp, %rbp
-	xorl	%eax, %eax           
-	xorps	%xmm1, %xmm1          
+	movq	%rsp, %rbp          
+	xorl	%eax, %eax              ; clean %rax
+	xorps	%xmm1, %xmm1            ; clean %xmm1
 	ucomiss	%xmm0, %xmm1            ; 0 - x
-	ja	LBB0_3                    
-	movl	$1, %eax              
+	ja	LBB0_3                      ; if x < 0  goto LBB0_3 
+	movl	$1, %eax                 
 	ucomiss	%xmm1, %xmm0            ; x - 0
-	jne	LBB0_2                     
-	jnp	LBB0_3
+	jne	LBB0_2                      ; if x > 0 || NaN  
+	jnp	LBB0_3                      ; if PF != 1    not NaN this is x == 0
 LBB0_2:                             ; x > 0 || NAN
-	xorps	%xmm1, %xmm1            
-	xorl	%eax, %eax             
+	xorps	%xmm1, %xmm1            ; clean %xmm1 
+	xorl	%eax, %eax              ; clean %rax
 	ucomiss	%xmm1, %xmm0            ; x - 0
 	setbe	%al                     ; NAN then set al == 1 
 	orl	$2, %eax                    ; 2 | 1 == 3 %eax is 3     
